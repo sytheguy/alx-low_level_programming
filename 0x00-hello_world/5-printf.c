0a740ea111c3214c4754bc6d6cb74bf06bd6c734
sytheguy <syboakye295@gmail.com>
2023-03-16 05:09:36 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main fuction for the task at hand
+ *
+ * Return: 0 when the code works succesfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
