b79415f63c506067f4c5b7fd5546a3ca60a9fdf5
sytheguy <syboakye295@gmail.com>
2023-03-16 18:28:18 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,17 @@
 /**
  * main - main function
  *
- * Return: 0 on success
+ * Return:  0 for success
  */
 
 int main(void)
 {
-  printf("Size of char: %ld byte(s)\n", sizeof(char));
-  printf("Size of short: %ld byte(s)\n", sizeof(short));
-  printf("Size of int: %ld byte(s)\n", sizeof(int));
-  printf("Size of long: %ld byte(s)\n", sizeof(long));
-  printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-  printf("Size of float: %ld byte(s)\n", sizeof(float));
-  printf("Size of double: %ld byte(s)\n", sizeof(double));
-  printf("Size of long double: %ld byte(s)\n", sizeof(long double));
-  printf("Size of pointer: %ld byte(s)\n", sizeof(void *));
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of short: %ld byte(s)\n", sizeof(char));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long: %ld byte(s)\n", sizeof(long));
+	printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
 
-  return (0);
+	return (0);
 }
