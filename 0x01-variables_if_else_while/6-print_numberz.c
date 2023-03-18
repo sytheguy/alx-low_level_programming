13d85350c453e822cc2a1118e212953a4278f649
sytheguy <syboakye295@gmail.com>
2023-03-17 18:54:07 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - prints numbers from 0-9
  *
  * Return: always 0
  */
@@ -11,7 +11,9 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
