6b5a0697c6f7c655d46a3ef648ef691d2f0a5b56
sytheguy <syboakye295@gmail.com>
2023-03-17 19:06:45 -0700

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main function 
+ * main - main function
  *
  * Return: always 0
  */
@@ -10,16 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 48 ; i < 58 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
-		{	
-			putchar(i + '0');
+		putchar(i);
+		if (i != 57)
+		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
