1f489f2483fade3ab1683dbf9a404cf4cd7c499a
sytheguy <syboakye295@gmail.com>
2023-03-17 19:39:07 -0700

    two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,12 +17,12 @@ int main(void)
 			if (m > n)
 			{
 				putchar(n);
-					putchar(m);
-					if (n != 56 || m != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
