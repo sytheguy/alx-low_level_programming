43c327d6d703ba3dc9706cf6b2d33d3d972e6647
sytheguy <syboakye295@gmail.com>
2023-04-14 05:37:25 -0700

    new commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,25 +2,16 @@
 #include "main.h"
 
 /**
- * main - Prints the number of arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0
+ * main - Prints the number of arguments passed to the program.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to the command-line arguments.
+ *
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
-	return (0);
+    (void)argv; /* Ignore argv */
+    printf("%d\n", argc - 1);
+    return (0);
 }
